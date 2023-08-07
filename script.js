9f65c4f18c6e458200b8b8f32d909fbea3961d13
SyedYaseenUddin <57957691+SyedYaseenUddin@users.noreply.github.com>
2023-08-07 13:44:07 +0530

    3 files modified

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
-console.log('Hello!');
+let map = null;
+const generateMap = (geojson, home) => {
+  map = L.map('leaflet-map').setView(home, 13);
+
+  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution:
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  }).addTo(map);
+  L.geoJSON(geojson).addTo(map);
+  setTimeout(() => {
+    window.dispatchEvent(new Event('resize'));  
+  }, 3000)
+};
+
+const generateGeojson = (coor) => {
+  return {
+    type: 'FeatureCollection',
+    features: [
+      {
+        type: 'Feature',
+        geometry: {
+          type: 'Polygon',
+          coordinates: [coor],
+        },
+        properties: {},
+      },
+    ],
+  };
+};
+
+document.querySelector('#submitBtn').addEventListener('click', () => {
+  let coordinate = document.querySelector('#coordinate').value;
+  coordinate = JSON.parse(coordinate).map((d) => d.split(',').map((d) => +d));
+  const geojson = generateGeojson(coordinate);
+  let home = JSON.parse(JSON.stringify(coordinate[0])).reverse();
+  document.querySelector('#map-area').innerHTML =
+    '<div id="leaflet-map"></div>';
+  generateMap(geojson, home);
+});

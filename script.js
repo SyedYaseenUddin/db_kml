b36d4e126229bce2dd400897bd5345d6aa4af159
SyedYaseenUddin <57957691+SyedYaseenUddin@users.noreply.github.com>
2023-08-08 13:41:03 +0530

    2 files modified, 2 added

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+import * as geoJsonArea from './lib/geoarea.js';
 let map = null;
 const generateMap = (geojson, home) => {
   map = L.map('map').setView(home, 18);
@@ -32,10 +33,15 @@ document.querySelector('#submitBtn').addEventListener('click', () => {
   let coordinate = document.querySelector('#coordinate').value;
   coordinate = JSON.parse(coordinate).map((d) => d.split(',').map((d) => +d));
   const geojson = generateGeojson(coordinate);
+  debugger;
+  let measured_area = geoJsonArea.geometry(geojson.features[0].geometry);
+  measured_area = +(measured_area / 4047).toFixed(3) + ' Acres';
+  console.log();
   let home = JSON.parse(JSON.stringify(coordinate[0])).reverse();
   document.querySelector('#map-area').innerHTML = `<div class="row">
   <div class="col-12">
     <div id="map"></div>
+    <h4>Total area : ${measured_area}</h4>
   </div>
 </div>`;
   setTimeout(() => generateMap(geojson, home));
